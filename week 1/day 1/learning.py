16cce06190b97a4a5c8ff688767ba6bba65d3ae0
ToobaMushtaq07 <mtooba237@gmail.com>
2026-07-21 12:08:58 +0500

    [ADD] week 1 task

---
+++ week 1/day 1/learning.py
@@ -0,0 +1,12 @@
+# true conditon
+a = int(input("enter first number: "))
+b = int(input("enter second number : "))
+print(a >= b)
+
+
+
+ #  find average
+a = int(input("enter first number: "))
+b = int(input("enter second number : "))
+average = ( a + b) / 2
+print("average =", average)
---
+++ week 1/day 1/task.py
@@ -0,0 +1,8 @@
+#swap two numb
+a = 12
+b = 15
+     a = a^b
+     b = a^b
+     a = a^b
+print("a =", a)
+print("b =", b)  
